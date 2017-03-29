fe0ba3790a94e4cd8b6adc2e0c584f9bdeb654ef
Shivani Pandya <spandya@csis.org>
2017-03-29 16:31:55 -0400

    SPP - redone tidy data w/updated MilCon as of 3/29

--- Data/merge.R
+++ Data/merge.R
@@ -5,14 +5,14 @@ library(openxlsx)
 #mg <- list.files(pattern =("*descriptions"))
 #for (i in 1:length(mg)) assign(mg[i], read.csv(mg[i]))
 
-setwd("H:/Crisis-Funding/Data/Tidy Data")
+setwd("H:/Crisis-Funding/Data/FY2017/Output")
 
 file_list <- list.files()
 
 dataset <- rbindlist(lapply( file_list, fread ), fill = TRUE)
 
-dataset <- dataset %>% select(-V1)
+# dataset <- dataset %>% select(-V1)
 
-write.csv(dataset, "2011-2017 Combined OCO Files.csv", append = FA)
+write.csv(dataset, "2017 OCO Merge.csv", append = FA)
 
 
