5f8b0423c15fe711312d1423e2f4d5cd46d5a591
Gregory Sanders <grysar@zort.net>
2019-01-15 12:06:28 -0500

    Reset and then trying to put all changes together after failed push.

--- Crisis_Funding_Data_Processing.R
+++ Crisis_Funding_Data_Processing.R
@@ -1,5 +1,5 @@
 # ---
-#   title: "Crisis Funding Exploration"
+#   title: "Crisis Funding Classification"
 # author: "Greg Sanders"
 # date: "March 28, 2017"
 # output:
@@ -17,107 +17,62 @@ library(Hmisc)
 library(csis360)
 
 
-# Path<-"K:\\2007-01 PROFESSIONAL SERVICES\\R scripts and data\\"
-# Path<-"C:\\Users\\Greg Sanders\\SkyDrive\\Documents\\R Scripts and Data SkyDrive\\"
-Path<-"C:\\Users\\gsand_000.ALPHONSE\\Documents\\Development\\R-scripts-and-data\\"
-source(paste(Path,"lookups.r",sep=""))
-source(paste(Path,"helper.r",sep="")) 
+source("https://raw.githubusercontent.com/CSISdefense/R-scripts-and-data/master/lookups.r")
+source("https://raw.githubusercontent.com/CSISdefense/R-scripts-and-data/master/helper.r")
 
-# diigtheme1:::diiggraph()
-
-Coloration<-read.csv(
-  paste(Path,"Lookups\\","lookup_coloration.csv",sep=""),
-  header=TRUE, sep=",", na.strings="", dec=".", strip.white=TRUE, 
-  stringsAsFactors=FALSE
-)
-
-Coloration<-ddply(Coloration
-                  , c(.(R), .(G), .(B))
-                  , transform
-                  , ColorRGB=as.character(
-                    if(min(is.na(c(R,G,B)))) {NA} 
-                    else {rgb(max(R),max(G),max(B),max=255)}
-                  )
-)
-
-
-axis.text.size<-12
-strip.text.size<-12
-legend.text.size<-8
-# table.text.size<-5.75
-title.text.size<-12
-geom.text.size<-12
-
-main.text.size<-2
-note.text.size<-1.40
 
 ##Import Data
 # read in detailed defense dataset    
-ZipFile<-unz(file.path("LargeDataSets","Defense_budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.zip"),
-             "Defense_budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.txt")
-defense_data <- readr::read_delim(ZipFile,
-  delim="\t",na=c("NULL","NA"))
-readr::problems(defense_data)
-defense_data<-standardize_variable_names(defense_data)
-
-defense_data<-defense_data[,!colnames(defense_data) %in%
-    c(
-      "ContractCrisisFunding.1"             ,
-      "localareasetaside.1",
-      "IsOMBocoList.1",
-      "ContractCrisisFunding_1"             ,
-      "localareasetaside_1",
-      "IsOMBocoList_1"
-    )]
-
-rm(ZipFile)
+# ZipFile<-unz(file.path("Data","Defense_budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.zip"),
+#              "Defense_budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.txt")
+# defense_data <- readr::read_delim(ZipFile,
+#   delim="\t",na=c("NULL","NA"))
+# readr::problems(defense_data)
+# defense_data<-standardize_variable_names(defense_data)
 
 # read in full data set    
-ZipFile<-unz(file.path("LargeDataSets","Overall_budget_SP_LocationVendorCrisisFundingHistoryBucketCustomer.zip"),
-             "Overall_budget_SP_LocationVendorCrisisFundingHistoryBucketCustomer.txt")
-full_data <- readr::read_delim(file.path("LargeDataSets",
-  "Overall_budget_SP_LocationVendorCrisisFundingHistoryBucketCustomer.txt"),
-                      delim="\t",
-  na=c("NULL","NA"))
-
+# ZipFile<-unz(file.path("Data","budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.zip"),
+#              "budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.txt")
+#Need to manually unzip ecause unz doesn't work above 2 gigs
+full_data <- readr::read_delim(file.path("Data",
+                                         "budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.txt"),
+                               delim="\t",
+                               na=c("NULL","NA"))
 
 full_data<-standardize_variable_names(full_data)
-missingf<-colnames(full_data)[!colnames(full_data) %in% colnames(defense_data)]
-missingd<-colnames(defense_data)[!colnames(defense_data) %in% colnames(full_data)]
 
+full_data<-full_data[,!colnames(full_data) %in%
+                             c(
+                               "ContractCrisisFunding.1"             ,
+                               "localareasetaside.1",
+                               "IsOMBocoList.1",
+                               "ContractCrisisFunding_1"             ,
+                               "localareasetaside_1",
+                               "IsOMBocoList_1"
+                             )]
 
-
-
-defense_data$CrisisFundingLegacy<-defense_data$CrisisFunding
-
-defense_data<-apply_lookups(Path,defense_data)
-defense_data<-subset(defense_data, year(Fiscal.Year)>=2000)
-
-full_data$CrisisFundingLegacy<-full_data$CrisisFunding
-full_data<-apply_lookups(Path,full_data)
+full_data$ContractingCustomer<-factor(full_data$ContractingCustomer)
 full_data<-subset(full_data, year(Fiscal.Year)>=2000)
+full_data<-apply_lookups(Path,full_data)
 
-defense_data$Theater<-as.character(defense_data$CrisisFundingTheater)
-defense_data$Theater[defense_data$Theater %in% c("Afghanistan","Iraq")]<-"Afghanistan and Iraq"
-defense_data$International<-defense_data$Theater
-defense_data$International[defense_data$Theater %in% c("Afghanistan and Iraq",
-  "Regional Support",
-  "Rest of World")]<-"International"
-defense_data$International<-ordered(defense_data$International,
-  levels=c("International",
-    "Domestic"))
+#Create new  variables
+full_data$CrisisFundingLegacy<-full_data$CrisisFunding
 
 full_data$Theater<-as.character(full_data$CrisisFundingTheater)
 full_data$Theater[full_data$Theater %in% c("Afghanistan","Iraq")]<-"Afghanistan and Iraq"
 full_data$International<-full_data$Theater
 full_data$International[full_data$Theater %in% c("Afghanistan and Iraq",
-  "Regional Support",
-  "Rest of World")]<-"International"
+                                                 "Regional Support",
+                                                 "Rest of World")]<-"International"
 full_data$International<-ordered(full_data$International,
-  levels=c("International",
-    "Domestic"))
+                                 levels=c("International",
+                                          "Domestic"))
 
 
+
+
+
+# rm(ZipFile)
 
 
 
@@ -155,7 +110,6 @@ full_data<-csis360::read_and_join(
   skip_check_var=c("NIAcrisisFunding","IsHurricane")
 )
 
-
 full_data$SubCustomer<-full_data$ContractingSubCustomer
 full_data<-csis360::read_and_join(
   full_data,
@@ -179,62 +133,13 @@ full_data<-csis360::read_and_join(
   skip_check_var=c("SAMcrisisFunding")
 )
 
-
-
-defense_data<-csis360::read_and_join(
-  defense_data,
-  "Lookup_ContingencyHumanitarianPeacekeepingOperation.csv",
-  by="ContingencyHumanitarianPeacekeepingOperation",
-  # replace_na_var=NULL,
-  # overlap_var_replaced=TRUE,
-  # add_var="Is.Defense"
-  # new_var_checked=TRUE,
-  skip_check_var="CHPKisCrisisFunding"
-)
-
-colnames(defense_data)[colnames(defense_data)=="ContractingCustomer"]<-"Customer"
-defense_data<-csis360::read_and_join(
-  defense_data,
-  "LOOKUP_Customer.csv",
-  by="Customer",
-  # replace_na_var=NULL,
-  # overlap_var_replaced=TRUE,
-  add_var="Is.Defense",
-  new_var_checked="Is.Defense"
-  # skip_check_var=NULL
-)
-
-defense_data<-csis360::read_and_join(
-  defense_data,
-  "Lookup_nationalinterestactioncode.csv",
-  by="nationalinterestactioncode",
-  # replace_na_var=NULL,
-  # overlap_var_replaced=TRUE,
-  # add_var="Is.Defense"
-  # new_var_checked=TRUE,
-  skip_check_var=c("NIAcrisisFunding","IsHurricane")
-)
-
-
-defense_data$SubCustomer<-defense_data$ContractingSubCustomer
-defense_data<-csis360::read_and_join(
-  defense_data,
-  "Lookup_SubCustomer.csv",
-  by=c("Customer","SubCustomer"),
-  # replace_na_var=NULL,
-  # overlap_var_replaced=TRUE,
-  add_var="SubCustomer.detail"
-  # new_var_checked=TRUE,
-  # skip_check_var=c("NIAcrisisFunding","IsHurricane")
-)
-
-
 full_data$DecisionTree<-factor(full_data$DecisionTree)
 full_data$DecisionTreeStep4<-factor(full_data$DecisionTreeStep4)
 
 
-save(full_data,defense_data,
-  file="LargeDataSets//budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.Rdata")
+
+save(full_data,
+  file="Data//budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.Rdata")
 
 # load(file="budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.Rdata")
 
