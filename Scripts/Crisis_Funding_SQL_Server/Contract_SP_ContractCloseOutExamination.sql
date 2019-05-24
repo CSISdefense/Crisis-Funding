94a83d4e373d2534f428633b2842a42a14644eb8
Gregory Sanders <gsanders@csis.org>
2019-05-23 23:17:59 -0400

    More terminated details and adding is closed out.

--- Scripts/Crisis_Funding_SQL_Server/Contract_SP_ContractCloseOutExamination.sql
+++ Scripts/Crisis_Funding_SQL_Server/Contract_SP_ContractCloseOutExamination.sql
@@ -13,7 +13,7 @@ GO
 -- Create date: 2/01/2013
 -- Description:	Break down contracts by size.
 -- =============================================
-ALTER PROCEDURE [Contract].[SP_ContractTerminationExamination]
+CREATE PROCEDURE [Contract].[SP_ContractCloseOutExamination]
 	-- Add the parameters for the stored procedure here
 	--@IsDefense varchar(255)
 	--@ServicesOnly Bit
@@ -28,18 +28,16 @@ BEGIN
 
 	 
 		select  cc.CSIScontractID
-		,max(cc.isDefaultOrCause) as isDefaultOrCause
-,sum(iif(f.SignedDate<cc.MaxTerminatedDate,ObligatedAmount,0)) as ObligatedBeforeMaxTerminatedDate
-,sum(iif(f.SignedDate=cc.MaxTerminatedDate,ObligatedAmount,0)) as ObligatedOnMaxTerminatedDate
-,sum(iif(f.SignedDate>cc.MaxTerminatedDate,ObligatedAmount,0)) as ObligatedAfterMaxTerminatedDate
-,max(cc.MaxTerminatedDate) as MaxTerminatedDate
+		,max(cc.IsClosed) as IsClosed
+,max(cc.MaxClosedDate) as MaxClosedDate
+,sum(iif(f.SignedDate>cc.MaxClosedDate,ObligatedAmount,0)) as ObligatedAfterMaxClosedDate
 from contract.fpds f
 inner join contract.CSIStransactionID ct
 on ct.CSIStransactionID=f.CSIStransactionID
 inner join contract.ContractDiscretization cc
 on ct.CSIScontractID=cc.CSIScontractID
---Terminated Contracts Only
-where cc.IsTerminated=1 
+--Closed Out Contracts Only
+where  cc.IsClosed =1 
 group by cc.CSIScontractID
 	END
 	
