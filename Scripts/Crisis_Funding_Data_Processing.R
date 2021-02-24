a78cb51061fc51da490cc477d63d31628cdf84ed
Greg Sanders <gregory.sanders@gmail.com>
2021-02-24 18:56:57 -0500

    Minor cleanup with the new file.

--- Scripts/Crisis_Funding_Data_Processing.R
+++ Scripts/Crisis_Funding_Data_Processing.R
@@ -15,7 +15,7 @@ library(dplyr)
 library(reshape2)
 library(Hmisc)
 library(csis360)
-
+library(readr)
 
 source("https://raw.githubusercontent.com/CSISdefense/R-scripts-and-data/master/lookups.r")
 source("https://raw.githubusercontent.com/CSISdefense/R-scripts-and-data/master/helper.r")
@@ -34,10 +34,15 @@ source("https://raw.githubusercontent.com/CSISdefense/R-scripts-and-data/master/
 # ZipFile<-unz(file.path("Data","budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.zip"),
 #              "budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.txt")
 #Need to manually unzip ecause unz doesn't work above 2 gigs
-full_data <- readr::read_delim(file.path("Data",
-                                         "Budget.SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.txt"),
-                               delim="\t",
-                               na=c("NULL","NA"))
+full_data<-read_tsv(file.path("data","semi_clean",
+                              "budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.txt"),
+                    na = c("NA","NULL"),
+                    guess_max = 10000000)
+
+#Dropping time to complete
+if(all(is.na((full_data[nrow(full_data),]))))
+  full_data<-full_data[1:(nrow(full_data)-1),]
+
 
 full_data<-standardize_variable_names(full_data)
 
@@ -58,7 +63,6 @@ full_data<-subset(full_data, Fiscal.Year>=2000 & Fiscal.Year<=2017)
 #Create new  variables
 full_data$CrisisFundingLegacy<-full_data$CrisisFunding
 
-full_data$Theater<-as.character(full_data$CrisisFundingTheater)
 full_data$Theater[full_data$Theater %in% c("Afghanistan","Iraq")]<-"Afghanistan and Iraq"
 
 
@@ -76,73 +80,75 @@ summary(full_data$International)
 
 
 
+full_data$ContingencyHumanitarianPeacekeepingOperation[full_data$ContingencyHumanitarianPeacekeepingOperation==""]<-NA
+full_data<-read_and_join_experiment(full_data,
+  lookup_file="ContingencyHumanitarianPeacekeepingOperation.csv",
+  by=c("ContingencyHumanitarianPeacekeepingOperation"),
+  path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+  dir="assistance//",
+  add_var = "ContingencyHumanitarianPeacekeepingOperationText")
 
-# rm(ZipFile)
-
-
-
-full_data<-read_and_join(
+# colnames(full_data)[colnames(full_data)=="ContractingCustomer"]<-"Customer"
+full_data<-read_and_join_experiment(
   full_data,
-  "Lookup_ContingencyHumanitarianPeacekeepingOperation.csv",
-  by="ContingencyHumanitarianPeacekeepingOperation",
-  # replace_na_var=NULL,
-  # overlap_var_replaced=TRUE,
-  # add_var="Is.Defense"
-  # new_var_checked=TRUE,
-  skip_check_var="CHPKisCrisisFunding"
-  )
-
-colnames(full_data)[colnames(full_data)=="ContractingCustomer"]<-"Customer"
-full_data<-read_and_join(
-  full_data,
-  "LOOKUP_Customer.csv",
-  by="Customer",
-  # replace_na_var=NULL,
-  # overlap_var_replaced=TRUE,
+  "Customer.csv",
+  by=c("ContractingCustomer"="Customer"),
+  path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+  dir="office//",
   add_var="Is.Defense",
   new_var_checked="Is.Defense"
   # skip_check_var=NULL
 )
 
-full_data<-read_and_join(
+full_data$nationalinterestactioncode[full_data$nationalinterestactioncode==""]<-NA
+full_data<-read_and_join_experiment(
   full_data,
-  "Lookup_nationalinterestactioncode.csv",
-  by="nationalinterestactioncode",
-  # replace_na_var=NULL,
-  # overlap_var_replaced=TRUE,
-  # add_var="Is.Defense"
-  # new_var_checked=TRUE,
-  skip_check_var=c("NIAcrisisFunding","IsHurricane")
-)
+  "NationalInterestActionCode.csv",
+  by=c("nationalinterestactioncode"="NationalInterestActionCode"),
+  path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+  dir="assistance//",skip_check_var = c('IsHurricane',	'CrisisFunding'
+  ))
 
-full_data$SubCustomer<-full_data$ContractingSubCustomer
-full_data<-read_and_join(
+full_data<-read_and_join_experiment(
   full_data,
-  "Lookup_SubCustomer.csv",
-  by=c("Customer","SubCustomer"),
+  "SubCustomer.csv",
+  by=c("ContractingCustomer"="Customer","ContractingSubCustomer"="SubCustomer"),
+  path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+  dir="office//",
   # replace_na_var=NULL,
   # overlap_var_replaced=TRUE,
   add_var="SubCustomer.detail"
   # new_var_checked=TRUE,
   # skip_check_var=c("NIAcrisisFunding","IsHurricane")
 )
+full_data$SAMcrisisFunding[full_data$SAMcrisisFunding==""]<-NA
 
 full_data$CCRexception[full_data$CCRexception==""]<-NA
-full_data<-read_and_join(
+full_data<-read_and_join_experiment(
   full_data,
-  "Lookup_CCRexception.csv",
+  "CCRexception.csv",
   by="CCRexception",
   replace_na_var="CCRexception",
-  # overlap_var_replaced=TRUE,
-  # add_var="Is.Defense"
-  # new_var_checked=TRUE,
-  skip_check_var=c("SAMcrisisFunding")
-)
+  path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+  dir="assistance//",skip_check_var = c('SAMcrisisFunding'))
+  
+  
+#   full_data,
+#   "CCRexception.csv",
+#   by="CCRexception",
+#   replace_na_var="CCRexception",
+#   # overlap_var_replaced=TRUE,
+#   # add_var="Is.Defense"
+#   # new_var_checked=TRUE,
+#   skip_check_var=c("SAMcrisisFunding")
+# )
 
-full_data<-read_and_join(full_data,
-                      "Lookup_SQL_CompetitionClassification.csv",
+full_data<-read_and_join_experiment(full_data,
+                      "CompetitionClassification.csv",
                       by=c("CompetitionClassification",
                            "ClassifyNumberOfOffers"),
+                      path="https://raw.githubusercontent.com/CSISdefense/Lookup-Tables/master/",
+                      dir="contract//",
                       add_var="No.Competition.Offer")
 
 
@@ -151,26 +157,21 @@ full_data<-read_and_join(full_data,
 full_data<-replace_nas_with_unlabeled(full_data,"ContractCrisisFunding")
 full_data<-replace_nas_with_unlabeled(full_data,"Is.Defense")
 full_data<-replace_nas_with_unlabeled(full_data,"Theater")
-full_data<-replace_nas_with_unlabeled(full_data,"contingencyhumanitarianpeacekeepingoperationText")
-full_data$SAMcrisisFunding[full_data$SAMcrisisFunding==""]<-NA
+full_data<-replace_nas_with_unlabeled(full_data,"ContingencyHumanitarianPeacekeepingOperationText")
 
 
+colnames(full_data)[colnames(full_data)=="Fiscal.Year"]<-"fiscal_year"
 full_data<-deflate(full_data,
-                   money_var = "Action.Obligation",
-                   deflator_var="Deflator.2017"
+                   money_var = "Action_Obligation",
+                   deflator_var="GDPdeflator2017"
 )
 
+colnames(full_data)[colnames(full_data)=="Action_Obligation_GDPdeflator2017"]<-"Obligation.2017"
 
-summary(factor(full_data$CompetitionClassification))
-
-full_data$NoCompOffr<-as.character(full_data$No.Competition.sum)
-full_data$NoCompOffr<-as.character(full_data$NoCompOffr)
-full_data$NoCompOffr[full_data$ClassifyNumberOfOffers %in% c("5-9 Offers","10-24 Offers","25-99 Offers","100+ Offers")
-                     &  full_data$NoCompOffr == "2+ Offers"] <-"5+ Offers"
-full_data$NoCompOffr[full_data$ClassifyNumberOfOffers %in% c("Two Offers","3-4 Offers")
-                     &  full_data$NoCompOffr == "2+ Offers"] <-"2-4 Offers"
-
+summary(factor(full_data$No.Competition.Offer))
+full_data$NoCompOffr<-as.character(full_data$No.Competition.Offer)
 full_data$NoCompOffr[full_data$IsUrgency==1]<-"Urgency"
+full_data$NoCompOffr[full_data$CompetitionClassification=="Unlabeled: Competition; Unlabeled Offers"]<-"Unlabeled"
 full_data$NoCompOffr<-factor(full_data$NoCompOffr)
 
 summary(full_data$NoCompOffr)
@@ -191,8 +192,9 @@ full_data$NoCompOffr<-factor(full_data$NoCompOffr,c(
     "5+ Offers",
   "Unlabeled"
   ))
-full_data %>% group_by(NoCompOffr) %>% dplyr::summarise(obligation.2016=sum(Obligation.2016,na.rm=TRUE))
+full_data %>% group_by(NoCompOffr) %>% dplyr::summarise(Obligation.2017=sum(Obligation.2017,na.rm=TRUE))
 
+full_data$dFYear<-as.Date(paste("1/1/",as.character(full_data$Fiscal.Year),sep=""),"%m/%d/%Y")
 
 for(i in 1:ncol(full_data))
   if (typeof(full_data[,i])=="character")
@@ -202,7 +204,7 @@ full_labels_and_colors<-prepare_labels_and_colors(full_data,na_replaced=TRUE)
 full_column_key<-get_column_key(full_data)
 
 save(full_data,full_labels_and_colors,full_column_key,
-  file="Data//budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.Rdata")
+  file="Data//semi_clean//budget_SP_LocationVendorCrisisFundingHistoryBucketCustomerDetail.Rdata")
 
 
 
